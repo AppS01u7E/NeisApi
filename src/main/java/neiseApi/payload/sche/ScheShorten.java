8f8a0acc0d1380b07cbf2d1aba84e3ea0471bddb
jinu <jinwoo794533@gmail.com>
2021-11-08 19:52:54 +0900

    :zap: getter

--- src/main/java/neiseApi/payload/sche/ScheShorten.java
+++ src/main/java/neiseApi/payload/sche/ScheShorten.java
@@ -2,18 +2,42 @@ package neiseApi.payload.sche;
 
 public class ScheShorten {
     private final int totalCount;
-    private final String day;
-    private final String grade;
-    private final String classNum;
-    private final String period;
+    private final int day;
+    private final int grade;
+    private final int classNum;
+    private final int period;
     private final String subject;
 
-    public ScheShorten(int totalCount, String day, String grade, String classNum, String period, String subject) {
+    public ScheShorten(int totalCount, int day, int grade, int classNum, int period, String subject) {
         this.totalCount = totalCount;
         this.day = day;
         this.grade = grade;
         this.classNum = classNum;
         this.period = period;
         this.subject = subject;
+    }
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public int getClassNum() {
+        return classNum;
+    }
+
+    public int getPeriod() {
+        return period;
+    }
+
+    public String getSubject() {
+        return subject;
     }
 }
