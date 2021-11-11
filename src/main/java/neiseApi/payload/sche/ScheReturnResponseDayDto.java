510965cb9bbf932fe33399fee427efebc50bf24b
jinu <jinwoo794533@gmail.com>
2021-11-11 11:27:24 +0900

    :zap: ScheReturn per Day

---
+++ src/main/java/neiseApi/payload/sche/ScheReturnResponseDayDto.java
@@ -0,0 +1,81 @@
+package neiseApi.payload.sche;
+
+
+import java.util.List;
+
+public class ScheReturnResponseDayDto {
+
+    private int grade;
+    private int classNum;
+    private int totalCount;
+    private int day;
+    private List<Subject> subjects;
+
+    public ScheReturnResponseDayDto(int grade, int classNum, int totalCount, int day, List<Subject> subjects) {
+        this.grade = grade;
+        this.classNum = classNum;
+        this.totalCount = totalCount;
+        this.day = day;
+        this.subjects = subjects;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public int getClassNum() {
+        return classNum;
+    }
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public List<Subject> getSubjects() {
+        return subjects;
+    }
+
+    public ScheReturnResponseDayDto setGrade(int grade) {
+        this.grade = grade;
+        return this;
+    }
+
+    public ScheReturnResponseDayDto setClassNum(int classNum) {
+        this.classNum = classNum;
+        return this;
+    }
+
+    public ScheReturnResponseDayDto setTotalCount(int totalCount) {
+        this.totalCount = totalCount;
+        return this;
+    }
+
+    public ScheReturnResponseDayDto setDay(int day) {
+        this.day = day;
+        return this;
+    }
+
+    public ScheReturnResponseDayDto setSubjects(List<Subject> subjects) {
+        this.subjects = subjects;
+        return this;
+    }
+
+
+    public static class Subject {
+        private String name;
+        private int period;
+
+        public Subject(String subject, int period) {
+            this.name = subject;
+            this.period = period;
+        }
+
+    }
+
+
+
+}
