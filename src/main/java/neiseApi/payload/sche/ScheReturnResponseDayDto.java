4c5fe45c98be4abf4cef749e18b5f839eda63579
jinu <jinwoo794533@gmail.com>
2021-11-11 13:31:21 +0900

    :bug: test

--- src/main/java/neiseApi/payload/sche/ScheReturnResponseDayDto.java
+++ src/main/java/neiseApi/payload/sche/ScheReturnResponseDayDto.java
@@ -6,21 +6,20 @@ import java.util.List;
 
 public class ScheReturnResponseDayDto {
 
-    private int grade;
-    private int classNum;
-    private int totalCount;
-    private int day;
+    private final int grade;
+    private final int classNum;
+    private final int totalCount;
+    private final int day;
     private List<Subject> subjects = new ArrayList<>();
 
     public static class Subject {
-        private String name;
-        private int period;
+        private final String name;
+        private final int period;
 
-        public Subject(String subject, int period) {
-            this.name = subject;
+        public Subject(String name, int period) {
+            this.name = name;
             this.period = period;
         }
-
     }
 
 
@@ -50,31 +49,6 @@ public class ScheReturnResponseDayDto {
 
     public List<Subject> getSubjects() {
         return subjects;
-    }
-
-    public ScheReturnResponseDayDto setGrade(int grade) {
-        this.grade = grade;
-        return this;
-    }
-
-    public ScheReturnResponseDayDto setClassNum(int classNum) {
-        this.classNum = classNum;
-        return this;
-    }
-
-    public ScheReturnResponseDayDto setTotalCount(int totalCount) {
-        this.totalCount = totalCount;
-        return this;
-    }
-
-    public ScheReturnResponseDayDto setDay(int day) {
-        this.day = day;
-        return this;
-    }
-
-    public ScheReturnResponseDayDto setSubjects(List<Subject> subjects) {
-        this.subjects = subjects;
-        return this;
     }
 
 }
