ae6541260f123bb54e1b111518eec101eb0f0f68
jinu <jinwoo794533@gmail.com>
2021-11-24 13:09:44 +0900

    :bug: index outbound Error

--- src/main/java/neiseApi/Neis.java
+++ src/main/java/neiseApi/Neis.java
@@ -139,14 +139,15 @@ public class Neis {
             List<ScheResponse.HisTimetable> timetable = mapper.readValue(url, ScheResponse.class).getHisTimetable();
 
             for (int i = 0; i <= 9; i++){
+                int j = i;
                 ScheResponse.HisTimetable.Row row = timetable.get(1).getRow().get(i);
                 try {
                     arrayList.add(new ScheShortenBlock(10,
-                            row.getaLL_TI_YMD(), row.getgRADE(), row.getcLASS_NM(), i += 1,
+                            row.getaLL_TI_YMD(), row.getgRADE(), row.getcLASS_NM(), j += 1,
                             row.getiTRT_CNTNT().isEmpty() ? "자습" : row.getiTRT_CNTNT()));
                 } catch (NullPointerException e){
                     arrayList.add(new ScheShortenBlock(10,
-                            row.getaLL_TI_YMD(), row.getgRADE(), row.getcLASS_NM(), i += 1,
+                            row.getaLL_TI_YMD(), row.getgRADE(), row.getcLASS_NM(), j += 1,
                             "자습"));
                 }
             }
