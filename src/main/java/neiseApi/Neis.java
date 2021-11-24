fdcff1de546526ff65d007a293bced58c8b782a4
jinu <jinwoo794533@gmail.com>
2021-11-24 13:16:16 +0900

    :bug:

--- src/main/java/neiseApi/Neis.java
+++ src/main/java/neiseApi/Neis.java
@@ -138,14 +138,16 @@ public class Neis {
         try{
             List<ScheResponse.HisTimetable> timetable = mapper.readValue(url, ScheResponse.class).getHisTimetable();
 
-            for (int i = 0; i <= 9; i++){
-                int j = i;
+            for (int i = 0; i < 10; i++){
+
                 ScheResponse.HisTimetable.Row row = timetable.get(1).getRow().get(i);
                 try {
+                    int j = i;
                     arrayList.add(new ScheShortenBlock(10,
                             row.getaLL_TI_YMD(), row.getgRADE(), row.getcLASS_NM(), j += 1,
                             row.getiTRT_CNTNT().isEmpty() ? "자습" : row.getiTRT_CNTNT()));
                 } catch (NullPointerException e){
+                    int j = i;
                     arrayList.add(new ScheShortenBlock(10,
                             row.getaLL_TI_YMD(), row.getgRADE(), row.getcLASS_NM(), j += 1,
                             "자습"));
